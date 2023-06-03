d63ff251fd0401727b4a813f96d0ba35c418842b
0220482123031 <0220482123031@LAB1MAQ23>
2023-06-02 21:45:13 -0300

    Add project files.

---
+++ DevDay23T3.Domain/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDay23T3.Domain
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; } 
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string TaxId { get; set; } //"CPF"
+        public void seila(Guid id)
+        {
+            Console.WriteLine("id " + id);
+        }
+    }
+
+}
